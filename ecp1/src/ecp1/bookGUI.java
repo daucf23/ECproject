7b10ccb36d43ed91a3e29157bc1fadd6c1022c69
David Almeida <davidalmeida2@knights.ucf.edu>
2018-09-07 02:36:46 -0400

    added mouse listeners for buttons. need dialogue boxes now

--- ecp1/src/ecp1/bookGUI.java
+++ ecp1/src/ecp1/bookGUI.java
@@ -3,14 +3,19 @@ package ecp1;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
+import java.awt.Color;
 
 
 public class bookGUI {
 	private static JTextField numItems;
 	private static JTextField textField;
 	private static JTextField textField_1;
+	private static JTextField textField_2;
+	private static JTextField textField_3;
 	
 	
 	public static void main(String[] args)
@@ -20,8 +25,12 @@ public class bookGUI {
 	}
 	
 
+	
+
+
 	public static void bookGUI() {
 	JFrame gui = new JFrame();
+	gui.getContentPane().setBackground(Color.LIGHT_GRAY);
 	
 	gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	gui.setTitle("Book Store Galore");
@@ -32,66 +41,216 @@ public class bookGUI {
 	
 	
 	JPanel panel = new JPanel();
+	panel.setBackground(Color.DARK_GRAY);
 	panel.setBounds(0, 648, 704, 33);
 	gui.getContentPane().add(panel);
 	
 	int totalItems = 0;
 	
-	JButton btnNewButton = new JButton("Process Item " + itemCount);
-	btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
-	btnNewButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
+	JButton btnProcessItem = new JButton("Process Item " + itemCount);
+	btnProcessItem.setVerticalAlignment(SwingConstants.BOTTOM);
+	btnProcessItem.addMouseListener(new MouseListener() {
+
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
 		}
 	});
-	panel.add(btnNewButton);
+	panel.add(btnProcessItem);
 	
-	JButton btnNewButton_1 = new JButton("Confirm Item " + itemCount);
-	panel.add(btnNewButton_1);
+	JButton btnConfirmOrder = new JButton("Confirm Item " + itemCount);
+	btnConfirmOrder.addMouseListener(new MouseListener() {
+
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+	panel.add(btnConfirmOrder);
 	
 	JButton btnViewOrder = new JButton("View Order");
+	btnViewOrder.addMouseListener(new MouseListener() {
+
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
 	panel.add(btnViewOrder);
 	
 	JButton btnFinishOrder = new JButton("Finish Order");
+	btnFinishOrder.addMouseListener(new MouseListener() {
+
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
 	panel.add(btnFinishOrder);
 	
 	JButton btnNewOrder = new JButton("New Order");
+	btnNewOrder.addMouseListener(new MouseListener() {
+
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
 	panel.add(btnNewOrder);
 	
 		
-	JButton btnNewButton_2 = new JButton("Exit");
-	panel.add(btnNewButton_2);
+	JButton btnExit = new JButton("Exit");
+	btnExit.addMouseListener(new MouseListener() {
+
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			System.exit(0);
+		}
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+	panel.add(btnExit);
 	
 	JLabel lblNumberOfItems = new JLabel("Number of Items in this Order:");
-	lblNumberOfItems.setBounds(143, 204, 146, 33);
+	lblNumberOfItems.setBounds(143, 204, 212, 33);
 	gui.getContentPane().add(lblNumberOfItems);
 	
 	numItems = new JTextField();
-	numItems.setBounds(299, 210, 86, 20);
+	numItems.setBounds(365, 210, 259, 20);
 	gui.getContentPane().add(numItems);
 	numItems.setColumns(10);
 	
+	
 	JLabel lblNewLabel = new JLabel("Book ID for Item " + itemCount +":");
-	lblNewLabel.setBounds(197, 231, 92, 14);
+	lblNewLabel.setBounds(197, 266, 158, 14);
 	gui.getContentPane().add(lblNewLabel);
 	
 	JLabel lblNewLabel_1 = new JLabel("Enter Quantity for Item " + itemCount + ":");
-	lblNewLabel_1.setBounds(163, 249, 126, 14);
+	lblNewLabel_1.setBounds(163, 319, 192, 14);
 	gui.getContentPane().add(lblNewLabel_1);
 	
 	JLabel lblNewLabel_2 = new JLabel("Item " + itemCount + " Info:");
-	lblNewLabel_2.setBounds(236, 322, 146, 14);
+	lblNewLabel_2.setBounds(231, 365, 124, 14);
 	gui.getContentPane().add(lblNewLabel_2);
 	
 	textField = new JTextField();
-	textField.setBounds(299, 228, 86, 20);
+	textField.setBounds(365, 263, 259, 20);
 	gui.getContentPane().add(textField);
 	textField.setColumns(10);
 	
 	textField_1 = new JTextField();
-	textField_1.setBounds(299, 246, 86, 20);
+	textField_1.setBounds(365, 316, 259, 20);
 	gui.getContentPane().add(textField_1);
 	textField_1.setColumns(10);
-	totalItems = Integer.parseInt(numItems.getText());
+	
+	textField_2 = new JTextField();
+	textField_2.setBounds(365, 362, 259, 20);
+	gui.getContentPane().add(textField_2);
+	textField_2.setColumns(10);
+	
+	JLabel lblOrderSubtotalFor = new JLabel("Order Subtotal for "+ itemCount + " Items:");
+	lblOrderSubtotalFor.setBounds(158, 419, 197, 14);
+	gui.getContentPane().add(lblOrderSubtotalFor);
+	
+	textField_3 = new JTextField();
+	textField_3.setBounds(365, 416, 259, 20);
+	gui.getContentPane().add(textField_3);
+	textField_3.setColumns(10);
+	//totalItems = Integer.parseInt(numItems.getText());
 	
 	
 	
